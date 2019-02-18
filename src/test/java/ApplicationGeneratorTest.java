d548aedd4f956fb322b6ddb18d05a9a0411e03f8
Noric Couderc <noric.couderc@cs.lth.se>
2019-02-18 17:04:18 +0100

    Added test for SetApplicationGenerator
    The test checks that the seed is correct and that applications are generated for
    the needed types.

--- src/test/java/ApplicationGeneratorTest.java
+++ src/test/java/ApplicationGeneratorTest.java
@@ -4,6 +4,7 @@ import se.lth.cs.Application;
 import se.lth.cs.ApplicationGeneration.ApplicationGenerator;
 import se.lth.cs.ApplicationGeneration.ListApplicationGenerator;
 import se.lth.cs.ApplicationGeneration.MapApplicationGenerator;
+import se.lth.cs.ApplicationGeneration.SetApplicationGenerator;
 
 import java.util.List;
 
@@ -38,6 +39,21 @@ public class ApplicationGeneratorTest {
         Assert.assertEquals(apps.get(2).getDataStructureName(), "java.util.IdentityHashMap");
         Assert.assertEquals(apps.get(3).getDataStructureName(), "java.util.LinkedHashMap");
         Assert.assertEquals(apps.get(4).getDataStructureName(), "java.util.WeakHashMap");
+    }
 
+    @Test
+    public void testSetApplicationGenerator() {
+        ApplicationGenerator g = new SetApplicationGenerator();
+        List<Application<?>> apps = g.createApplications(0, 1, 10);
+        // Test all generated applications have the same seed
+        for (Application<?> app : apps) {
+            Assert.assertEquals(0, app.getSeed());
+            Assert.assertEquals(10, app.getSize());
+        }
+        Assert.assertEquals(apps.get(0).getDataStructureName(), "java.util.HashSet");
+        Assert.assertEquals(apps.get(1).getDataStructureName(), "java.util.TreeSet");
+        Assert.assertEquals(apps.get(2).getDataStructureName(), "java.util.LinkedHashSet");
+        Assert.assertEquals(apps.get(3).getDataStructureName(), "java.util.concurrent.ConcurrentSkipListSet");
+        Assert.assertEquals(apps.get(4).getDataStructureName(), "java.util.concurrent.CopyOnWriteArraySet");
     }
 }
