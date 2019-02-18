43af59e852eb55bfb6e4d2b58d793f683cc11665
Noric Couderc <noric.couderc@cs.lth.se>
2019-02-18 16:58:32 +0100

    Added test for MapApplicationGenerator
    The test checks that the seed is correct and that applications are generated for
    the needed types.

--- src/test/java/ApplicationGeneratorTest.java
+++ src/test/java/ApplicationGeneratorTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import se.lth.cs.Application;
 import se.lth.cs.ApplicationGeneration.ApplicationGenerator;
 import se.lth.cs.ApplicationGeneration.ListApplicationGenerator;
+import se.lth.cs.ApplicationGeneration.MapApplicationGenerator;
 
 import java.util.List;
 
@@ -21,5 +22,22 @@ public class ApplicationGeneratorTest {
         Assert.assertEquals(apps.get(0).getDataStructureName(), "java.util.ArrayList");
         Assert.assertEquals(apps.get(1).getDataStructureName(), "java.util.LinkedList");
         Assert.assertEquals(apps.get(2).getDataStructureName(), "java.util.Vector");
+    }
+
+    @Test
+    public void testMapApplicationGenerator() {
+        ApplicationGenerator g = new MapApplicationGenerator();
+        List<Application<?>> apps = g.createApplications(0, 1, 10);
+        // Test all generated applications have the same seed
+        for (Application<?> app : apps) {
+            Assert.assertEquals(0, app.getSeed());
+            Assert.assertEquals(10, app.getSize());
+        }
+        Assert.assertEquals(apps.get(0).getDataStructureName(), "java.util.HashMap");
+        Assert.assertEquals(apps.get(1).getDataStructureName(), "java.util.TreeMap");
+        Assert.assertEquals(apps.get(2).getDataStructureName(), "java.util.IdentityHashMap");
+        Assert.assertEquals(apps.get(3).getDataStructureName(), "java.util.LinkedHashMap");
+        Assert.assertEquals(apps.get(4).getDataStructureName(), "java.util.WeakHashMap");
+
     }
 }
