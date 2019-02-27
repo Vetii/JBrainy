caf87972859b9f8822d7089b2b2a8cc5331c3aea
Noric Couderc <noric.couderc@cs.lth.se>
2019-02-27 15:46:56 +0100

    Added test for CounterSpecification
    Tests that when a counter is added to the specification, it is indeed added.

--- src/test/kotlin/CounterSpecificationTest.kt
+++ src/test/kotlin/CounterSpecificationTest.kt
@@ -1,5 +1,6 @@
 import org.junit.Assert
 import org.junit.Test
+import papi.Constants
 import se.lth.cs.CounterSpecification
 import java.io.StringReader
 
@@ -10,5 +11,15 @@ class CounterSpecificationTest {
         val s = ""
         val result = CounterSpecification.fromReader(StringReader(s))
         Assert.assertTrue(result.currentSpec.isEmpty())
+    }
+
+    /**
+     * Test that the spec is created with the counter passed in the reader
+     */
+    @Test
+    fun testOneCounterSpec() {
+        val s = "PAPI_L1_DCM"
+        val result = CounterSpecification.fromReader(StringReader(s))
+        Assert.assertEquals(mapOf(Pair("PAPI_L1_DCM", Constants.PAPI_L1_DCM)), result.currentSpec)
     }
 }
