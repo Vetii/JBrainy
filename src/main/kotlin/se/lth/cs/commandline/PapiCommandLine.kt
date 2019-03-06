73327d9d2d7bce4507e307018d9192ce3ebc401c
Noric Couderc <noric.couderc@cs.lth.se>
2019-03-06 11:28:14 +0100

    Added option to select the number of runs when running a PAPI Run

--- src/main/kotlin/se/lth/cs/commandline/PapiCommandLine.kt
+++ src/main/kotlin/se/lth/cs/commandline/PapiCommandLine.kt
@@ -3,6 +3,7 @@ package se.lth.cs.commandline
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
 import se.lth.cs.CounterSpecification
 import se.lth.cs.JMHProcessor
 import se.lth.cs.PapiRunner
@@ -19,6 +20,11 @@ class PapiCommandLine : CliktCommand() {
 
     val outputFileName : String by option("-o", help="The output file for PAPI counter data")
             .default("hardware-perf-data.csv")
+
+    val numberRuns : Int by option("--number-runs", "-n",
+            help="Number of runs per counter and application")
+            .int()
+            .default(100)
 
     fun checkFileExists(file : File) {
         if (!file.exists()) {
@@ -38,7 +44,7 @@ class PapiCommandLine : CliktCommand() {
         val r = PapiRunner(counterSpec)
 
         val jmhData = JMHProcessor().process(inputFile)
-        val features = r.processJMHData(1, jmhData)
+        val features = r.processJMHData(numberRuns, jmhData)
         outputFile.writeText(r.featuresToCSV(features))
     }
 }
